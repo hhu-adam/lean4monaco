69a6173b4a451c49551c61fcb9af36a3166e6590
Alexander Bentkamp <bentkamp@gmail.com>
2024-07-13 22:11:02 +0200

    wait for blue underline

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('Editor Test', () => {
   it('displays the editor', () => {
     cy.visit('http://localhost:5173/')
     cy.contains('#check Nat')
-    cy.wait(5000);
+    cy.get('.squiggly-info') // blue underline under #check
     cy.iframe().contains('Nat : Type')
   })
 })
