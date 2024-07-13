13ab3c4272775bdec7cc2f359890fb5181452a22
Alexander Bentkamp <bentkamp@gmail.com>
2024-07-13 15:12:59 +0200

    e2e test

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,29 @@
+const getIframeDocument = () => {
+  return cy
+  .get('iframe')
+  // Cypress yields jQuery element, which has the real
+  // DOM element under property "0".
+  // From the real DOM iframe element we can get
+  // the "document" element, it is stored in "contentDocument" property
+  // Cypress "its" command can access deep properties using dot notation
+  // https://on.cypress.io/its
+  .its('0.contentDocument').should('exist')
+}
+
+const getIframeBody = () => {
+  // get the document
+  return getIframeDocument()
+  // automatically retries until body is loaded
+  .its('body').should('not.be.undefined')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  .then(cy.wrap)
+}
+
+describe('Editor Test', () => {
+  it('displays the editor', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('#check Nat')
+    getIframeBody().contains('Nat : Type')
+  })
+})
