2857c1c4d23626313c2118f558da0cd659f8a731
Alexander Bentkamp <bentkamp@gmail.com>
2024-07-13 22:14:41 +0200

    add comment

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('Editor Test', () => {
   it('displays the editor', () => {
     cy.visit('http://localhost:5173/')
     cy.contains('#check Nat')
-    cy.get('.squiggly-info') // blue underline under #check
+    cy.get('.squiggly-info') // blue underline under #check (This is important to wait for before checking the infoview.)
     cy.iframe().contains('Nat : Type')
   })
 })
