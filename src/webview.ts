1b48638921639143730d7d0bcb32e00a454671b4
Alexander Bentkamp <bentkamp@gmail.com>
2024-07-16 12:26:14 +0200

    load infoview from unpkg.com/

--- src/webview.ts
+++ src/webview.ts
@@ -1,15 +1,24 @@
 //@ts-nocheck
 import type { EditorApi } from '@leanprover/infoview'
-import { renderInfoview } from '@leanprover/infoview'
+import { loadRenderInfoview } from '@leanprover/infoview/loader'
 import { Rpc } from './monaco-lean4/vscode-lean4/src/rpc'
 
-const vscodeApi = window.parent // acquireVsCodeApi()
+const vscodeApi = window.parent
 
 const rpc = new Rpc((m: any) => vscodeApi.postMessage(JSON.stringify(m)))
-window.addEventListener('message', e => rpc.messageReceived(JSON.parse(e.data)))
+window.addEventListener('message', e => {rpc.messageReceived(JSON.parse(e.data))})
 const editorApi: EditorApi = rpc.getApi()
 
 const div: HTMLElement | null = document.querySelector('#react_root')
 
-const api = renderInfoview(editorApi, div!)
-rpc.register(api)
+if (div) {
+    const imports = 
+     {
+     '@leanprover/infoview': 'https://unpkg.com/@leanprover/infoview/dist/index.production.min.js',
+     'react': 'https://unpkg.com/@leanprover/infoview/dist/react.production.min.js',
+     'react/jsx-runtime': 'https://unpkg.com/@leanprover/infoview/dist/react-jsx-runtime.production.min.js',
+     'react-dom': 'https://unpkg.com/@leanprover/infoview/dist/react-dom.production.min.js',
+     }
+    console.log(imports)
+    loadRenderInfoview(imports, [editorApi, div], api => rpc.register(api))
+}
