2b9859b6dcce0f2de2cb44e339bed3a469738684
Jon Eugster <eugster.jon@gmail.com>
2024-08-24 00:53:40 +0200

    update workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   defaultCommandTimeout: 100000, // for firefox on linux
+  experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
