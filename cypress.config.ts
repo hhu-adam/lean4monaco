1e7e0f1f54cd051c4f47016b6535fd6c36da1d00
Alexander Bentkamp <bentkamp@gmail.com>
2025-06-17 15:30:42 +0200

    add ci test with fake user agent

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,12 @@ export default defineConfig({
   experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chromium' && process.env.CYPRESS_USER_AGENT) {
+          launchOptions.args.push(`--user-agent=${process.env.CYPRESS_USER_AGENT}`)
+          return launchOptions
+        }
+      })
     },
   },
 });
