b5017a43317b77c513761771905dd82c2d9f826d
Alexander Bentkamp <bentkamp@gmail.com>
2025-06-17 19:49:34 +0200

    improve test setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.name === 'chromium' && process.env.CYPRESS_USER_AGENT) {
-          launchOptions.args.push(`--user-agent=${process.env.CYPRESS_USER_AGENT}`)
+        if (browser.name === 'chromium' && process.env.CYPRESS_USER_AGENT_OS) {
+          launchOptions.args.push(`--user-agent=\"Mozilla/5.0 (${process.env.CYPRESS_USER_AGENT_OS}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36\"`)
           return launchOptions
         }
       })
