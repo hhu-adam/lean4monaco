3229a674a2e01d7372321aeede5011d7e4b11e24
Jon Eugster <eugster.jon@gmail.com>
2026-03-12 09:09:54 +0100

    chore: drop unused dependency

--- demo/src/LeanMonacoComponent.tsx
+++ demo/src/LeanMonacoComponent.tsx
@@ -25,7 +25,7 @@ export function LeanMonacoComponent({ numberEditors } : { numberEditors: number}
     })()
 
     return () => {
-      _leanMonaco.dispose()
+      leanMonaco?.dispose()
     }
   }, [leanMonacoOptions, setLeanMonaco])
 
