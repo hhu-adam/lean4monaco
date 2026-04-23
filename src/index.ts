dfa609f58f921bb4bb1bceee2b1a92d174a1e34a
Jon Eugster <eugster.jon@gmail.com>
2026-04-24 00:53:53 +0200

    fix: add an export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './editor'
 export * from './leanmonaco'
 
 export { RpcSessionAtPos } from './vscode-lean4/vscode-lean4/src/infoview'
+export { DocumentPosition } from './vscode-lean4/lean4-infoview/src/infoview/util'
 export { LeanClient } from './vscode-lean4/vscode-lean4/src/leanclient'
