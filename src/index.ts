3c03b833dcf8a293722db9da38bf4bad2ad389c1
Jon Eugster <eugster.jon@gmail.com>
2026-04-24 21:49:01 +0200

    expose WithRpcSessions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export * from './editor'
 export * from './leanmonaco'
 
+// TODO: I think this should be present in the infoview-api and can be removed here
 export { RpcSessionAtPos } from './vscode-lean4/vscode-lean4/src/infoview'
+
 export { DocumentPosition } from './vscode-lean4/lean4-infoview/src/infoview/util'
 export { LeanClient } from './vscode-lean4/vscode-lean4/src/leanclient'
+export { WithRpcSessions } from './vscode-lean4/lean4-infoview/src/infoview/rpcSessions'
